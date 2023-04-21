4041dde13181fb8c907f6ae1ba556972ad5c6f45
Pablo <pablo.aledo@seqera.io>
2023-04-21 09:13:09 +0200

    add operations from operation type

--- graphcontraints/graph.py
+++ graphcontraints/graph.py
@@ -2,14 +2,46 @@ import networkx as nx
 import z3 as z3
 from enum import Enum
 
+class OperationTypes(Enum):
+    OPEN = 1
+    BOUNDED = 2
+    CLOSE = 3
+    UNBOUNDED = 4
+
 class Operations(Enum):
     OPEN = 1
-    WRITE = 2
     CLOSE = 3
+    WRITE = 4
+    READ = 5
+    SEEK = 6
+    TRUNCATE = 7
+    UNLINK = 8
+    RENAME = 9
+    MKDIR = 10
+    RMDIR = 11
+    SYMLINK = 12
+    LINK = 13
+    CHMOD = 14
+    CHOWN = 15
+    UTIMENS = 16
+    CREATE = 17
+    RELEASE = 18
+    FSYNC = 19
+    SETXATTR = 20
+    GETXATTR = 21
+    LISTXATTR = 22
+    REMOVEXATTR = 23
+    OPENDIR = 24
+
+def add_operations_from_types(solver, graph):
+    for node in graph.nodes():
+        solver.add( z3.Implies( graph.nodes()[node]['operation_type'] == OperationTypes.OPEN.value, graph.nodes()[node]['operation'] == Operations.OPEN.value ) )
+        solver.add( z3.Implies( graph.nodes()[node]['operation_type'] == OperationTypes.CLOSE.value, graph.nodes()[node]['operation'] == Operations.CLOSE.value ) )
+        solver.add( z3.Implies( graph.nodes()[node]['operation_type'] == OperationTypes.CLOSE.value, z3.And(graph.nodes()[node]['operation'] >= Operations.CLOSE.value, graph.nodes()[node]['operation'] <= Operations.CLOSE.value ) ) )
 
 def add_operation_constraints(solver, graph):
     for node in graph.nodes():
-        solver.add( graph.nodes()[node]['operation'] >= Operations.OPEN.value, graph.nodes()[node]['operation'] <= Operations.CLOSE.value )
+        solver.add( graph.nodes()[node]['operation_type'] >= OperationTypes.OPEN.value, graph.nodes()[node]['operation_type'] <= OperationTypes.CLOSE.value )
         solver.add( graph.nodes()[node]['transaction'] >= 0, graph.nodes()[node]['transaction'] <= 100 )
 
 def add_time_constraints(solver, graph):
@@ -24,11 +56,11 @@ def add_if_open_then_close(solver, graph):
            nodep  = node2
            nodesn = othernodes - {node2}
            andcondition = True
-           andcondition = z3.And( andcondition, z3.And(graph.nodes()[nodep]['operation'] == Operations.CLOSE.value, graph.nodes()[nodep]['transaction'] == graph.nodes()[node1]['transaction']) )
+           andcondition = z3.And( andcondition, z3.And(graph.nodes()[nodep]['operation_type'] == OperationTypes.CLOSE.value, graph.nodes()[nodep]['transaction'] == graph.nodes()[node1]['transaction']) )
            for node3 in nodesn:
-               andcondition = z3.And(andcondition, z3.Or(graph.nodes()[node3]['operation'] != Operations.CLOSE.value, z3.And(graph.nodes()[node3]['operation'] == Operations.CLOSE.value, graph.nodes()[node3]['transaction'] != graph.nodes()[node1]['transaction']) ) )
+               andcondition = z3.And(andcondition, z3.Or(graph.nodes()[node3]['operation_type'] != OperationTypes.CLOSE.value, z3.And(graph.nodes()[node3]['operation_type'] == OperationTypes.CLOSE.value, graph.nodes()[node3]['transaction'] != graph.nodes()[node1]['transaction']) ) )
            orcondition = z3.Or(orcondition, andcondition)
-       solver.add( z3.Implies( graph.nodes()[node1]['operation'] == Operations.OPEN.value, orcondition ) )
+       solver.add( z3.Implies( graph.nodes()[node1]['operation_type'] == OperationTypes.OPEN.value, orcondition ) )
 
 def add_if_op_then_open(solver, graph):
     for node1 in graph.nodes():
@@ -38,36 +70,36 @@ def add_if_op_then_open(solver, graph):
             nodep  = node2
             nodesn = othernodes - {node2}
             andcondition = True
-            andcondition = z3.And( andcondition, z3.And(graph.nodes()[nodep]['operation'] == Operations.OPEN.value, graph.nodes()[nodep]['transaction'] == graph.nodes()[node1]['transaction']) )
+            andcondition = z3.And( andcondition, z3.And(graph.nodes()[nodep]['operation_type'] == OperationTypes.OPEN.value, graph.nodes()[nodep]['transaction'] == graph.nodes()[node1]['transaction']) )
             # for node3 in nodesn:
-                # andcondition = z3.And(andcondition, z3.Or(graph.nodes()[node3]['operation'] != Operations.OPEN.value, z3.And(graph.nodes()[node3]['operation'] == Operations.OPEN.value, graph.nodes()[node3]['transaction'] != graph.nodes()[node1]['transaction']) ) )
+                # andcondition = z3.And(andcondition, z3.Or(graph.nodes()[node3]['operation_type'] != OperationTypes.OPEN.value, z3.And(graph.nodes()[node3]['operation_type'] == OperationTypes.OPEN.value, graph.nodes()[node3]['transaction'] != graph.nodes()[node1]['transaction']) ) )
             orcondition = z3.Or(orcondition, andcondition)
 
-        solver.add( z3.Implies( graph.nodes()[node1]['operation'] == Operations.WRITE.value, orcondition ) )
+        solver.add( z3.Implies( graph.nodes()[node1]['operation_type'] == OperationTypes.BOUNDED.value, orcondition ) )
 
 def add_close_after_open(solver, graph):
     for node1 in graph.nodes():
         othernodes = set(graph.adj[node1])
         for node2 in othernodes:
-            solver.add( z3.Implies( z3.And( graph.nodes()[node1]['operation'] == Operations.OPEN.value, graph.nodes()[node2]['operation'] == Operations.CLOSE.value, graph.nodes()[node1]['transaction'] == graph.nodes()[node2]['transaction'] ) , graph.nodes()[node1]['time'] < graph.nodes()[node2]['time'] ) )
+            solver.add( z3.Implies( z3.And( graph.nodes()[node1]['operation_type'] == OperationTypes.OPEN.value, graph.nodes()[node2]['operation_type'] == OperationTypes.CLOSE.value, graph.nodes()[node1]['transaction'] == graph.nodes()[node2]['transaction'] ) , graph.nodes()[node1]['time'] < graph.nodes()[node2]['time'] ) )
 
 def add_open_different_transactions(solver, graph):
     for node1 in graph.nodes():
         othernodes = set(graph.adj[node1])
         for node2 in othernodes:
-            solver.add( z3.Implies( z3.And( graph.nodes()[node1]['operation'] == Operations.OPEN.value, graph.nodes()[node2]['operation'] == Operations.OPEN.value ) , graph.nodes()[node1]['transaction'] != graph.nodes()[node2]['transaction'] ) )
+            solver.add( z3.Implies( z3.And( graph.nodes()[node1]['operation_type'] == OperationTypes.OPEN.value, graph.nodes()[node2]['operation_type'] == OperationTypes.OPEN.value ) , graph.nodes()[node1]['transaction'] != graph.nodes()[node2]['transaction'] ) )
 
 def add_op_after_open(solver, graph):
     for node1 in graph.nodes():
         othernodes = set(graph.adj[node1])
         for node2 in othernodes:
-            solver.add( z3.Implies( z3.And( graph.nodes()[node1]['operation'] == Operations.OPEN.value, graph.nodes()[node2]['operation'] == Operations.WRITE.value, graph.nodes()[node1]['transaction'] == graph.nodes()[node2]['transaction']) , graph.nodes()[node1]['time'] < graph.nodes()[node2]['time'] ) )
+            solver.add( z3.Implies( z3.And( graph.nodes()[node1]['operation_type'] == OperationTypes.OPEN.value, graph.nodes()[node2]['operation_type'] == OperationTypes.BOUNDED.value, graph.nodes()[node1]['transaction'] == graph.nodes()[node2]['transaction']) , graph.nodes()[node1]['time'] < graph.nodes()[node2]['time'] ) )
 
 def add_op_before_close(solver, graph):
     for node1 in graph.nodes():
         othernodes = set(graph.adj[node1])
         for node2 in othernodes:
-            solver.add( z3.Implies( z3.And( graph.nodes()[node1]['operation'] == Operations.WRITE.value, graph.nodes()[node2]['operation'] == Operations.CLOSE.value, graph.nodes()[node1]['transaction'] == graph.nodes()[node2]['transaction'] ) , graph.nodes()[node1]['time'] < graph.nodes()[node2]['time'] ) )
+            solver.add( z3.Implies( z3.And( graph.nodes()[node1]['operation_type'] == OperationTypes.BOUNDED.value, graph.nodes()[node2]['operation_type'] == OperationTypes.CLOSE.value, graph.nodes()[node1]['transaction'] == graph.nodes()[node2]['transaction'] ) , graph.nodes()[node1]['time'] < graph.nodes()[node2]['time'] ) )
 
 def show_solution(model, graph):
     times = set()
@@ -79,14 +111,14 @@ def show_solution(model, graph):
             if model[ graph.nodes()[node]["time"] ].as_long() == t :
                 node = graph.nodes()[node]
                 time        = model[ node['time'] ].as_long()
-                operation   = model[ node['operation'] ].as_long()
+                operation_type   = model[ node['operation_type'] ].as_long()
                 transaction = model[ node['transaction'] ].as_long()
-                print(time, operation, transaction)
+                print(time, operation_type, transaction)
 
 def generate_nodes(n):
     ret = list()
     for x in range(0,n):
-        node = {"name": "node_%04d" % x, "time": z3.Int("time_%04d" % x), "operation": z3.Int("operation_%04d" % x), "transaction": z3.Int("transaction_%04d" % x) }
+        node = {"name": "node_%04d" % x, "time": z3.Int("time_%04d" % x), "operation_type": z3.Int("operation_type_%04d" % x), "operation": z3.Int("operation_%04d" % x), "transaction": z3.Int("transaction_%04d" % x) }
         ret.append(node)
     return ret
 
@@ -96,10 +128,10 @@ G = nx.Graph()
 num_nodes = 3
 
 # define the nodes
-# node1 = {"name": "node1", "time": z3.Int('time1'), "operation": z3.Int('operation1'), "transaction": z3.Int('transaction1')}
-# node2 = {"name": "node2", "time": z3.Int('time2'), "operation": z3.Int('operation2'), "transaction": z3.Int('transaction2')}
-# node3 = {"name": "node3", "time": z3.Int('time3'), "operation": z3.Int('operation3'), "transaction": z3.Int('transaction3')}
-# node4 = {"name": "node4", "time": z3.Int('time4'), "operation": z3.Int('operation4'), "transaction": z3.Int('transaction4')}
+# node1 = {"name": "node1", "time": z3.Int('time1'), "operation_type": z3.Int('operation1'), "transaction": z3.Int('transaction1')}
+# node2 = {"name": "node2", "time": z3.Int('time2'), "operation_type": z3.Int('operation2'), "transaction": z3.Int('transaction2')}
+# node3 = {"name": "node3", "time": z3.Int('time3'), "operation_type": z3.Int('operation3'), "transaction": z3.Int('transaction3')}
+# node4 = {"name": "node4", "time": z3.Int('time4'), "operation_type": z3.Int('operation4'), "transaction": z3.Int('transaction4')}
 nodes = generate_nodes(num_nodes)
 
 for n in range(0, num_nodes):
@@ -120,6 +152,7 @@ for i in range(0, num_nodes):
 # Create a solver
 solver = z3.Solver()
 
+add_operations_from_types(solver, G)
 add_operation_constraints(solver, G)
 add_time_constraints(solver, G)
 add_if_open_then_close(solver, G)
@@ -129,7 +162,7 @@ add_op_after_open(solver, G)
 add_op_before_close(solver, G)
 add_open_different_transactions(solver, G)
 
-solver.add( G.nodes()[2]["operation"] == 2 );
+solver.add( G.nodes()[2]["operation_type"] == 2 );
 
 
 # Check if the solver is satisfiable
