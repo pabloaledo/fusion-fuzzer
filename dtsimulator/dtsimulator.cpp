b89c40ba28b22c69fbbd41495b1e10f134c1b22f
Pablo <pablo.aledo@seqera.io>
2023-04-21 06:47:51 +0200

    more operations

--- dtsimulator/dtsimulator.cpp
+++ dtsimulator/dtsimulator.cpp
@@ -5,6 +5,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <sys/stat.h>
+#include <utime.h>
+
 #define MAX_LINE_LENGTH 1024
 
 int file_descriptor[100];
@@ -36,6 +39,147 @@ void execute_command(char* command) {
             close(file_descriptor[transaction]);
 
             break;
+
+        case 4:
+            printf("delete %d\n", transaction);
+
+            remove("file.txt");
+
+            break;
+        
+        case 5:
+            printf("read %d\n", transaction);
+
+            char data2[100];
+            read(file_descriptor[transaction], data2, 100);
+
+            break;
+
+        case 6:
+            printf("seek %d\n", transaction);
+
+            lseek(file_descriptor[transaction], 0, SEEK_SET);
+
+            break;
+        
+        case 7:
+            printf("truncate %d\n", transaction);
+
+            ftruncate(file_descriptor[transaction], 0);
+
+            break;
+        
+        case 8:
+            printf("sync %d\n", transaction);
+
+            fsync(file_descriptor[transaction]);
+
+            break;
+
+
+        case 9:
+            printf("rename %d\n", transaction);
+
+            rename("file.txt", "file2.txt");
+
+            break;
+
+        case 10:
+            printf("link %d\n", transaction);
+
+            link("file.txt", "file2.txt");
+
+            break;
+
+        case 11:
+            printf("unlink %d\n", transaction);
+
+            unlink("file2.txt");
+
+            break;
+
+        case 12:
+            printf("mkdir %d\n", transaction);
+
+            mkdir("dir", 0777);
+
+            break;
+
+        case 13:
+            printf("rmdir %d\n", transaction);
+
+            rmdir("dir");
+
+            break;
+
+        case 14:
+            printf("chdir %d\n", transaction);
+
+            chdir("dir");
+
+            break;
+
+        case 15:
+            printf("chmod %d\n", transaction);
+
+            chmod("file.txt", 0777);
+
+            break;
+
+        case 16:
+            printf("chown %d\n", transaction);
+
+            chown("file.txt", 1000, 1000);
+
+            break;
+        
+        case 17:
+            printf("utime %d\n", transaction);
+
+            struct utimbuf time;
+            time.actime = 1000;
+            time.modtime = 1000;
+            utime("file.txt", &time);
+
+            break;
+        
+        case 18:
+            printf("stat %d\n", transaction);
+
+            struct stat buf;
+            stat("file.txt", &buf);
+
+            break;
+
+        case 19:
+            printf("lstat %d\n", transaction);
+
+            struct stat buf2;
+            lstat("file.txt", &buf2);
+
+            break;
+
+        case 20:
+            printf("access %d\n", transaction);
+
+            access("file.txt", F_OK);
+
+            break;
+        case 21:
+            printf("dup %d\n", transaction);
+
+            dup(file_descriptor[transaction]);
+
+            break;
+
+        case 22:
+            printf("dup2 %d\n", transaction);
+
+            dup2(file_descriptor[transaction], 0);
+
+            break;
+
+
     }
 }
 
