b2865313c10b0b7c46038e72be64357f77dcd2ad
Pablo <pablo.aledo@seqera.io>
2023-04-26 05:29:39 +0200

    fix
    Signed-off-by: Pablo <pablo.aledo@seqera.io>

--- orchestrator/script.sh
+++ orchestrator/script.sh
@@ -11,7 +11,7 @@ echo "/test/bin/dtsimulator /test/test.pattern"                                 
 echo "ls -l | grep -v list | grep -v fake | awk '{\$1=\"\"; \$2=\"\"; \$3=\"\"; \$4=\"\"; \$6=\"\"; \$7=\"\"; \$8=\"\"; print}' > list" >> test/test.sh
 echo "FROM ubuntu:22.04" > test/Dockerfile
 echo "rm -fr *" > test/init.sh
-echo "/test/bin/dtsimulator /test/init.pattern" > test/init.sh
+echo "/test/bin/dtsimulator /test/init.pattern" >> test/init.sh
 rm -fr ~/workspace/fusionfs/tests/random_test
 cp -r test ~/workspace/fusionfs/tests/random_test
 (cd ~/workspace/fusionfs/tests; ./run_tests.sh --store=s3 --bucket=fusionfs-ci random_test)
