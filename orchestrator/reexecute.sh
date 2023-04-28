6c938a1f77cd71732139cd1da0160bcda01c4074
Pablo <pablo.aledo@seqera.io>
2023-04-28 15:25:31 +0200

    test
    Signed-off-by: Pablo <pablo.aledo@seqera.io>

--- orchestrator/reexecute.sh
+++ orchestrator/reexecute.sh
@@ -3,7 +3,7 @@ mkdir -p test/{local,bin}
 ( cd test/local;  ../../../dtsimulator/dtsimulator ../init.pattern )
 ( cd test/local;  ../../../dtsimulator/dtsimulator ../test.pattern )
 ( cd test/local; ls -l | grep -v list | grep -v md5s | awk '{$1=""; $2=""; $3=""; $4=""; $6=""; $7=""; $8=""; print}' > list )
-( cd test/local; md5sum * | grep -v md5s > md5s )
+( cd test/local; echo fake > fake; md5sum * | grep -v md5s > md5s )
 \cp ../dtsimulator/dtsimulator test/bin
 echo "/test/bin/dtsimulator /test/test.pattern"                                                                >  test/test.sh
 echo "ls -l | grep -v list | grep -v fake | awk '{\$1=\"\"; \$2=\"\"; \$3=\"\"; \$4=\"\"; \$6=\"\"; \$7=\"\"; \$8=\"\"; print}' > list" >> test/test.sh
@@ -14,7 +14,7 @@ echo "/test/bin/dtsimulator /test/init.pattern" >> test/init.sh
 rm -fr ~/workspace/fusionfs/tests/random_test
 cp -r test ~/workspace/fusionfs/tests/random_test
 (cd ~/workspace/fusionfs/tests; ./run_tests.sh --store=s3 --bucket=fusionfs-ci random_test)
-(cd ~/workspace/fusionfs/tests/random_test/external_docker_folder; md5sum $(ls * | grep -v fake) > md5s)
+(cd ~/workspace/fusionfs/tests/random_test/external_docker_folder; md5sum * > md5s)
 md51=$(md5sum ~/workspace/fusionfs/tests/random_test/local/md5s | awk '{print $1}')
 md52=$(md5sum ~/workspace/fusionfs/tests/random_test/external_docker_folder/md5s | awk '{print $1}')
 
