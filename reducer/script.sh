22e9751ac45b83d8aa0cf42f1f91f0260cd3e027
Pablo <pablo.aledo@seqera.io>
2023-05-03 12:35:08 +0200

    fix
    Signed-off-by: Pablo <pablo.aledo@seqera.io>

--- reducer/script.sh
+++ reducer/script.sh
@@ -3,17 +3,18 @@ exec_both(){
     ( cd test/local;  ../../../dtsimulator/dtsimulator ../init.pattern )
     ( cd test/local;  ../../../dtsimulator/dtsimulator ../test.pattern )
     ( cd test/local; ls -l | grep -v list | grep -v md5s | awk '{$1=""; $2=""; $3=""; $4=""; $6=""; $7=""; $8=""; print}' > list )
-    ( cd test/local; md5sum * | grep -v md5s > md5s )
+    ( cd test/local; echo fake > fake; md5sum * | grep -v md5s > md5s )
     \cp ../dtsimulator/dtsimulator test/bin
     echo "/test/bin/dtsimulator /test/test.pattern"                                                                >  test/test.sh
     echo "ls -l | grep -v list | grep -v fake | awk '{\$1=\"\"; \$2=\"\"; \$3=\"\"; \$4=\"\"; \$6=\"\"; \$7=\"\"; \$8=\"\"; print}' > list" >> test/test.sh
     echo "FROM ubuntu:22.04" > test/Dockerfile
     echo "find -not -iname fake -delete" > test/init.sh
     echo "/test/bin/dtsimulator /test/init.pattern" >> test/init.sh
+    [ -e ~/mycontainer2/tests/random_test/ ] && find ~/mycontainer2/tests/random_test/ -not -iname fake -delete
     rm -fr ~/workspace/fusionfs/tests/random_test
     cp -r test ~/workspace/fusionfs/tests/random_test
     (cd ~/workspace/fusionfs/tests; ./run_tests.sh --store=s3 --bucket=fusionfs-ci random_test)
-    (cd ~/workspace/fusionfs/tests/random_test/external_docker_folder; md5sum $(ls * | grep -v fake) > md5s)
+    (cd ~/workspace/fusionfs/tests/random_test/external_docker_folder; md5sum * > md5s)
     md51=$(md5sum ~/workspace/fusionfs/tests/random_test/local/md5s | awk '{print $1}')
     md52=$(md5sum ~/workspace/fusionfs/tests/random_test/external_docker_folder/md5s | awk '{print $1}')
 }
